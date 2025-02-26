1dcad56509f45eeed3a1d83d0f28a45158f02034
Jonathan Garcia <jonathanpog@gmail.com>
2025-02-26 00:13:08 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1>Products page</h1>
+      <Button>Teste</Button>
+    </div>
+  );
+};
+
+export default ProductsPage;
